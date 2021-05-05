ce7f1b4cd4bd8883b6a090925ac76197a9b3263c
ysmin709 <61577262+ysmin709@users.noreply.github.com>
2021-05-05 10:45:15 +0900

    프로그래머스 Level 1
    예산

---
+++ Budget.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int answer = 0;
+    int sum = 0;
+    
+    sort(d.begin(), d.end());
+    
+    for (int i = 0; i < d.size(); i++) {
+        sum += d[i];
+        if (sum > budget) {
+            answer = i;
+            break;
+        }
+        else if (sum <= budget) answer = i+1;
+    }
+    
+    return answer;
+}
