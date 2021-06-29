03741701c4543b8991e13f33b00df983c100aab9
ysmin709 <61577262+ysmin709@users.noreply.github.com>
2021-06-29 16:06:35 +0900

    기능개발

---
+++ Programmers Level 2/functionDevelopment.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+    int i = 0;
+    int cnt = 0;
+    
+    while (i < speeds.size()) {
+        if (progresses[i] >= 100 && cnt > 0) { cnt++; i++; }
+        else if (progresses[i] >= 100) { cnt++; i++; }
+        else if (progresses[i] < 100 && cnt > 0) {
+            answer.push_back(cnt);
+            cnt = 0;
+            for (int j = i; j < speeds.size(); j++) progresses[j] += speeds[j];
+        }
+        else {
+            for (int j = i; j < speeds.size(); j++) progresses[j] += speeds[j];
+        }
+    }
+    answer.push_back(cnt);
+    
+    return answer;
+}
