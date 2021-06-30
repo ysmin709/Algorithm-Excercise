2a70ab84b27d7a8c01098651bd0d6641e0e11446
ysmin709 <tjdals1668@kookmin.ac.kr>
2021-06-30 16:42:23 +0900

    피보나치 수

---
+++ Programmers Level 2/fibonacci.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    vector<int> fibo;
+    
+    for (int i = 0; i <= n; i++) {
+        if (i == 0) fibo.push_back(0);
+        else if (i == 1) fibo.push_back(1);
+        else fibo.push_back((fibo[i-1] + fibo[i-2]) % 1234567);
+    }
+    
+    answer = fibo[fibo.size()-1];
+    
+    return answer;
+}
