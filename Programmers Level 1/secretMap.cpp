6e42966302f50234e6efa53edbb144025f75dc25
ysmin709 <tjdals1668@kookmin.ac.kr>
2021-07-04 20:46:42 +0900

    비밀지도

--- Programmers Level 1/secretMap.cpp
+++ Programmers Level 1/secretMap.cpp
@@ -4,34 +4,37 @@
 using namespace std;
 
 vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
-    vector<string> answer(n, "");
-    vector<vector<int>> temp1(n, vector<int>(n, 0));
-    vector<vector<int>> temp2(n, vector<int>(n, 0));
+    vector<string> answer;
+    vector<string> temp1;
+    vector<string> temp2;
     
     for (int i = 0; i < n; i++) {
-        int j = n-1;
-        while(arr1[i] > 1) {
-            temp1[i][j] = arr1[i] % 2;
-            arr1[i] /= 2;
-            j--;
-        }
-        temp1[i][j] = arr1[i];
-    }
-    for (int i = 0; i < n; i++) {
-        int j = n-1;
-        while(arr2[i] > 1) {
-            temp2[i][j] = arr2[i] % 2;
-            arr2[i] /= 2;
-            j--;
-        }
-        temp2[i][j] = arr1[i];
-    }
-    
-    for (int i = 0; i < n; i++) {
+        string s = "";
         for (int j = 0; j < n; j++) {
-            if (temp1[i][j] || temp2[i][j]) answer[i] += "#";
-            else answer[i] += " ";
+            if (arr1[i] % 2) s += '1';
+            else s += '0';
+            arr1[i] /= 2;
         }
+        temp1.push_back(s);
+    }
+    
+    for (int i = 0; i < n; i++) {
+        string s = "";
+        for (int j = 0; j < n; j++) {
+            if (arr2[i] % 2) s += '1';
+            else s += '0';
+            arr2[i] /= 2;
+        }
+        temp2.push_back(s);
+    }
+    
+    for (int i = 0; i < n; i++) {
+        string s = "";
+        for (int j = n-1; j >= 0; j--) {
+            if (temp1[i][j] == '1' || temp2[i][j] == '1') s += '#';
+            else s += ' ';
+        }
+        answer.push_back(s);
     }
     
     return answer;
