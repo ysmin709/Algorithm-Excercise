3f1423d0dd6de6c50320a09bf3ce6b33f6ec0d6b
ysmin709 <tjdals1668@kookmin.ac.kr>
2021-08-03 18:34:22 +0900

    전화번호 목록

---
+++ Programmers Level 2/전화번호 목록/phoneNumberList.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+    bool answer = true;
+    sort(phone_book.begin(), phone_book.end());
+    
+    for (int i = 1; i < phone_book.size(); i++) {
+        if (phone_book[i].find(phone_book[i-1]) == 0) return false;
+    }
+    
+    return answer;
+}
