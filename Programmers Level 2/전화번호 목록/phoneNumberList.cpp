ed26bdb0d2a0ea9c3c560ce05ba1f7cbf2bf96a0
ysmin709 <61577262+ysmin709@users.noreply.github.com>
2021-08-04 16:34:04 +0900

    다음 큰 숫자

--- Programmers Level 2/전화번호 목록/phoneNumberList.cpp
+++ Programmers Level 2/전화번호 목록/phoneNumberList.cpp
@@ -6,10 +6,10 @@ using namespace std;
 
 bool solution(vector<string> phone_book) {
     bool answer = true;
-    sort(phone_book.begin(), phone_book.end());
+    sort(phone_book.begin(), phone_book.end()); // string을 사전순 정렬
     
-    for (int i = 1; i < phone_book.size(); i++) {
-        if (phone_book[i].find(phone_book[i-1]) == 0) return false;
+    for (int i = 1; i < phone_book.size(); i++) { 
+        if (phone_book[i].find(phone_book[i-1]) == 0) return false; // phone_book[i]가 phone_book[i-1]을 포함하고 있다면 false를 리턴
     }
     
     return answer;
