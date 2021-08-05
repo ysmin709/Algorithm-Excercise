daa49740d9fffe16021133b2de373130dc1df3c1
ysmin709 <tjdals1668@kookmin.ac.kr>
2021-08-05 23:13:23 +0900

    프린터

--- Programmers Level 2/프린터/printer.cpp
+++ Programmers Level 2/프린터/printer.cpp
@@ -10,16 +10,16 @@ int solution(vector<int> priorities, int location) {
     vector<int> temp(priorities.begin(), priorities.end());
     stack<int> stk;
     
-    sort(temp.rbegin(), temp.rend());
+    sort(temp.rbegin(), temp.rend()); // 내림차순 정렬
     
     int i = 0, j = 0;
     while(stk.size() != temp.size()) {
-        if (j == temp.size()) j = 0;
-        if (temp[i] == priorities[j]) { 
+        if (j == temp.size()) j = 0; // 우선순위에서 밀려서 지나친 것들을 처리
+        if (temp[i] == priorities[j]) { // 우선순위 순서대로 작업 수행
             stk.push(priorities[j]);
             i++;
             priorities[j] = 0;
-            if (priorities[location] == 0) break;
+            if (priorities[location] == 0) break; // 우선순위가 0이면 끝
         }
         j++;
     }
