26c15b64cc6bd79978b659e270418ccfdd570e82
ysmin709 <tjdals1668@kookmin.ac.kr>
2021-08-16 23:02:22 +0900

    주식가격

--- Programmers Level 2/주식 가격/stockPrice.cpp
+++ Programmers Level 2/주식 가격/stockPrice.cpp
@@ -1,4 +1,3 @@
-#include <string>
 #include <vector>
 #include <stack>
 
@@ -9,18 +8,17 @@ vector<int> solution(vector<int> prices) {
     stack<pair<int, int>> stk;
     
     stk.push(make_pair(prices[0], 0));
-    for (int i = 1; i < prices.size(); i++) {
-        if (stk.top().first > prices[i]) {
-            while (!stk.empty() && stk.top().first > prices[i]) {
+    for (int i = 1; i < prices.size(); i++) { // 초기값은 넣었기 때문에 1부터 순회
+        if (stk.top().first > prices[i]) { // 가격이 떨어졌는지 체크
+            while (!stk.empty() && stk.top().first > prices[i]) { // 가격이 떨어진 모든 시점 체크
                 answer[stk.top().second] = i - stk.top().second;
                 stk.pop();
             }
-            stk.push(make_pair(prices[i], i));
         }
-        else stk.push(make_pair(prices[i], i));
+        stk.push(make_pair(prices[i], i)); // 현재 값을 stack에 저장
     }
     
-    while (!stk.empty()) {
+    while (!stk.empty()) { // 가격이 떨어지지 않은 시점들 처리
         answer[stk.top().second] = prices.size() - 1 - stk.top().second;
         stk.pop();
     }
