0a40fc1647ab442326b6d41bbf8cee537b69e292
ysmin709 <tjdals1668@kookmin.ac.kr>
2021-08-17 18:46:46 +0900

    짝지어 제거하기

--- Programmers Level 2/짝지어 제거하기/removePair.cpp
+++ Programmers Level 2/짝지어 제거하기/removePair.cpp
@@ -6,10 +6,10 @@ int solution(string s) {
     stack<char> stk;
     
     for (int i = 0; i < s.size(); i++) {
-        if (stk.empty() || stk.top() != s[i]) stk.push(s[i]);
+        if (stk.empty() || stk.top() != s[i]) stk.push(s[i]); // 스택이 비어있거나 스택의 top이 현재 값과 같을 때 push
         else stk.pop();
     }
     
-    if (stk.empty()) return 1;
-    else return 0;
+    if (stk.empty()) return 1; // 스택이 비어있으면 전부 제거한 것이므로 1을 리턴한다.
+    else return 0; // 더 이상 제거할 수 없으면 0을 리턴
 }
