f7312f72b6079dcfe3919cd9e22ea3d0e5dc8437
ysmin709 <61577262+ysmin709@users.noreply.github.com>
2021-08-18 20:19:47 +0900

    땅따먹기

---
+++ Programmers Level 2/땅따먹기/eatTheGround.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<vector<int>> land) {
+    for (int i = 0; i < land.size()-1; i++) {
+        land[i+1][0] += max(land[i][1], max(land[i][2], land[i][3]));
+        land[i+1][1] += max(land[i][0], max(land[i][2], land[i][3]));
+        land[i+1][2] += max(land[i][0], max(land[i][1], land[i][3]));
+        land[i+1][3] += max(land[i][0], max(land[i][1], land[i][2]));
+    }
+    return max(land[land.size()-1][0], max(land[land.size()-1][1], max(land[land.size()-1][2], land[land.size()-1][3])));
+}
