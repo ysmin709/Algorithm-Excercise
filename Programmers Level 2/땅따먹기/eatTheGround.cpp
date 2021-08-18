8e9f6c0b1767fe233fbbf2571bde0694b343d022
ysmin709 <61577262+ysmin709@users.noreply.github.com>
2021-08-18 20:20:10 +0900

    땅따먹기

--- Programmers Level 2/땅따먹기/eatTheGround.cpp
+++ Programmers Level 2/땅따먹기/eatTheGround.cpp
@@ -1,14 +1,21 @@
 #include <vector>
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
 int solution(vector<vector<int>> land) {
     for (int i = 0; i < land.size()-1; i++) {
-        land[i+1][0] += max(land[i][1], max(land[i][2], land[i][3]));
-        land[i+1][1] += max(land[i][0], max(land[i][2], land[i][3]));
-        land[i+1][2] += max(land[i][0], max(land[i][1], land[i][3]));
-        land[i+1][3] += max(land[i][0], max(land[i][1], land[i][2]));
+        for (int j = 0; j < 4; j++) {
+            int max = 0;
+            for (int k = 0; k < 4; k++) {
+                if (k == j) continue; // 같은 열은 제외
+                else if (land[i][k] > max) max = land[i][k]; // 3개의 열 중 가장 큰 값
+            }
+            land[i+1][j] += max; // 해당 위치로 갔을 때 가장 큰 값을 더해준다.
+        }
     }
-    return max(land[land.size()-1][0], max(land[land.size()-1][1], max(land[land.size()-1][2], land[land.size()-1][3])));
+    
+    int max = 0;
+    for (int i = 0; i < 4; i++) if (land[land.size()-1][i] > max) max = land[land.size()-1][i]; // 제일 마지막 행 중 가장 큰 값을 찾는다.
+
+    return max;
 }
