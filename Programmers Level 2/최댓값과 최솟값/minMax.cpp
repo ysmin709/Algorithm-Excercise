34ce984e30a792e43c33a16e6a153544fd81ca03
ysmin709 <tjdals1668@kookmin.ac.kr>
2021-08-31 00:04:41 +0900

    최댓값과 최솟값

--- Programmers Level 2/최댓값과 최솟값/minMax.cpp
+++ Programmers Level 2/최댓값과 최솟값/minMax.cpp
@@ -9,15 +9,15 @@ string solution(string s) {
     
     for (int i = 0; i < s.size(); i++) {
         string temp = "";
-        if (s[i] == ' ') continue;
+        if (s[i] == ' ') continue; // 숫자만 넣기 위해 공백일 때는 넘긴다.
         else if (s[i] >= '0' && s[i] <= '9') {
             while(s[i] >= '0' && s[i] <= '9') {
                 temp += s[i];
                 i++;
             }
-            arr.push_back(stoi(temp));
+            arr.push_back(stoi(temp)); // string을 int형으로 변환하여 vector에 넣는다.
         }
-        else if (s[i] == '-') {
+        else if (s[i] == '-') { // 음수 처리
             temp += '-';
             i++;
             while(s[i] >= '0' && s[i] <= '9') {
@@ -28,13 +28,13 @@ string solution(string s) {
         }
     }
     
-    int min = arr[0], max = arr[0];
+    int min = arr[0], max = arr[0]; // min, max값 체크
     for (int i = 0; i < arr.size(); i++) {
         if (arr[i] < min) min = arr[i];
         if (arr[i] > max) max = arr[i];
     }
     
-    answer += to_string(min);
+    answer += to_string(min); // string을 리턴해야 하기 때문에 string으로 변환
     answer += ' ';
     answer += to_string(max);
     
