bb2c386b7e4a207611935fccb51183ffd3d77fcc
ysmin709 <tjdals1668@kookmin.ac.kr>
2021-09-30 04:00:32 +0900

    2748 : 피보나치 수 2

---
+++ Baekjoon/DynamicProgramming/2748 피보나치 수 2/2748.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> vec(n+1, 0);
+
+    vec[0] = 0; vec[1] = 1;
+
+    for (int i = 2; i <= n; i++) vec[i] = vec[i-1] + vec[i-2];
+
+    cout << vec[n] << endl;
+}
