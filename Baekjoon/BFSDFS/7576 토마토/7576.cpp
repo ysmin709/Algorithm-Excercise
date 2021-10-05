d5ab8233e8bb35495620487fd9e8be4a9a4d08c7
ysmin709 <tjdals1668@kookmin.ac.kr>
2021-10-05 23:20:20 +0900

    7576 : 토마토

--- Baekjoon/BFSDFS/7576 토마토/7576.cpp
+++ Baekjoon/BFSDFS/7576 토마토/7576.cpp
@@ -15,7 +15,7 @@ void BFS() {
         int y = q.front().second;
         q.pop();
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 4; i++) { // 상하좌우 체크
             int nx = x + dx[i];
             int ny = y + dy[i];
 
@@ -36,9 +36,10 @@ int main() {
         for (int j = 0; j < m; j++) {
             cin >> arr[i][j];
             day[i][j] = -1;
+
             if (arr[i][j] == 1) {
-                q.push(make_pair(i, j));
-                day[i][j] = 0;
+                q.push(make_pair(i, j)); // 1인 좌표 전부 q에 push
+                day[i][j] = 0; // 1은 0일에 해당되므로 0으로 초기화
             }
         }
     }
@@ -46,14 +47,14 @@ int main() {
     BFS();
 
     int result = 0;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) { // 날짜의 최댓값 
         for (int j = 0; j < m; j++) {
             if (result < day[i][j]) result = day[i][j];
         }
     }
 
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+        for (int j = 0; j < m; j++) { // 방문해야 하는 노드 중에 0이 있는지 확인
             if (arr[i][j] == 0 && day[i][j] == -1) result = -1;
         }
     }
