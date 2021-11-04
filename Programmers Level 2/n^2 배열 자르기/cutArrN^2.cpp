4e56c034e860a7a9f06ee65eeef185144e946bef
ysmin709 <61577262+ysmin709@users.noreply.github.com>
2021-11-04 18:34:34 +0900

    n^2 배열 자르기

---
+++ Programmers Level 2/n^2 배열 자르기/cutArrN^2.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, long long left, long long right) {
+    vector<int> answer;
+    
+    for (long long i = left; i <= right; i++) {
+        long long tempI = i / n; // 행 인덱스
+        long long tempJ = i % n; // 열 인덱스
+        answer.push_back(max(tempI, tempJ)+1); // 인덱스 값 중 큰 값의 1을 더한 값을 push한다.
+    }
+    
+    return answer;
+}
