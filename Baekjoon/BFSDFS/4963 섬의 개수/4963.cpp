f008e1df222e1eedd22f605447ac3e174721e646
ysmin709 <61577262+ysmin709@users.noreply.github.com>
2021-11-08 14:14:23 +0900

    4963 : 섬의 개수

--- Baekjoon/BFSDFS/4963 섬의 개수/4963.cpp
+++ Baekjoon/BFSDFS/4963 섬의 개수/4963.cpp
@@ -3,26 +3,25 @@
 #include <cstring>
 using namespace std;
 
-int m, n, answer = 0;
+int m, n;
 int dx[] = {-1, 0, 1, -1, 1, -1, 0, 1};
 int dy[] = {-1, -1, -1, 0, 0, 1, 1, 1};
-int arr[50][50];
-bool visited[50][50];
+int arr[51][51];
+bool visited[51][51];
 queue<pair<int, int>> q;
 
 void bfs(pair<int, int> node) {
     q.push(node);
-    visited[node.first][node.second];
+    visited[node.first][node.second] = true;
 
     while(!q.empty()) {
-        int x = q.front().first;
-        int y = q.front().second;
+        node = q.front();
         q.pop();
-        visited[x][y] = true;
 
-        for (int k = 0; k < 8; k++) {
-            int nx = x + dx[k];
-            int ny = y + dy[k];
+        for (int i = 0; i < 8; i++) { // 8방향 체크
+            int nx = node.first + dx[i];
+            int ny = node.second + dy[i];
+
             if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
                 if (arr[nx][ny] && !visited[nx][ny]) {
                     q.push(make_pair(nx, ny));
@@ -30,17 +29,17 @@ void bfs(pair<int, int> node) {
                 }
             }
         }
-        cout << x << " " << y << endl;
     }
 }
 
 int main() {
     while(true) {
-        memset(arr, 0, sizeof(arr));
+        int answer = 0;
+        memset(arr, 0, sizeof(arr)); // 입력마다 arr와 visited 배열 초기화
         memset(visited, false, sizeof(visited));
 
         cin >> m >> n;
-        if (m == 0 && n == 0) return 0;
+        if (m == 0 && n == 0) return 0; // 입력이 0 0이면 프로그램 종료
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) cin >> arr[i][j];
@@ -48,7 +47,7 @@ int main() {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (arr[i][j] && !visited[i][j]) { bfs(make_pair(i, j)); answer++; }
+                if (arr[i][j] && !visited[i][j]) { bfs(make_pair(i, j)); answer++; } // 섬의 개수 체크
             }
         }
         cout << answer << endl;
