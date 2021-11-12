782367a4fd2e19116c04e58ad9631a29d7a2de74
ysmin709 <tjdals1668@kookmin.ac.kr>
2021-11-12 20:19:08 +0900

    11052 : 카드 구매하기

--- Baekjoon/DynamicProgramming/11052 카드 구매하기/11052.cpp
+++ Baekjoon/DynamicProgramming/11052 카드 구매하기/11052.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 int main() {
@@ -12,8 +11,11 @@ int main() {
 
     for (int i = 1; i <= n; i++) cin >> vec[i];
 
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= i; j++) dp[i] = max(dp[i], dp[i] + vec[i]);
+    dp[1] = vec[1];
+
+    for (int i = 2; i <= n; i++) {
+        // i장의 카드를 구하는 방법 중 가장 큰 값을 적용하여 N까지 구한다.
+        for (int j = 1; j <= i; j++) dp[i] = max(dp[i], dp[i-j] + vec[j]);
     }
 
     cout << dp[n] << endl;
