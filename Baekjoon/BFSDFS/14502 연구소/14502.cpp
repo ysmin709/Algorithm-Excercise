7b18da9ca4110691ab1de81b572d8caadd6cf463
ysmin709 <tjdals1668@kookmin.ac.kr>
2021-12-21 17:59:37 +0900

    14502 : 연구소

--- Baekjoon/BFSDFS/14502 연구소/14502.cpp
+++ Baekjoon/BFSDFS/14502 연구소/14502.cpp
@@ -1,93 +1,83 @@
-#include <iostream>
+#include <cstdio>
 #include <vector>
 #include <queue>
-#include <cstring>
 using namespace std;
 
-int n, m;
+int n, m, answer = 0;
 vector<vector<int>> arr(8, vector<int>(8, 0));
 vector<vector<int>> vec;
-bool visited[8][8];
+vector<pair<int, int>> virus;
 int dx[] = { -1, 0, 1, 0 };
 int dy[] = { 0, -1, 0, 1 };
 queue<pair<int, int>> q;
 
-void BFS(pair<int, int> node) {
-    q.push(node);
-    visited[node.first][node.second] = true;
+int BFS() {
+    int res = 0;
+    for (int i = 0; i < virus.size(); i++) q.push(virus[i]);
 
     while(!q.empty()) {
-        node = q.front();
+        int x = q.front().first;
+        int y = q.front().second;
         q.pop();
 
-        for (int i = 0; i < 4; i++) {
-            int nx = node.first + dx[i];
-            int ny = node.second + dy[i];
+        for (int i = 0; i < 4; i++) { // 4방향 체크
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
             if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
-                if (!vec[nx][ny] && !visited[nx][ny]) {
+                if (!vec[nx][ny]) {
                     vec[nx][ny] = 2;
                     q.push(make_pair(nx, ny));
-                    visited[nx][ny] = true;
                 }
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (!vec[i][j]) res++;
+        }
+    }
+
+    return res;
+}
+
+void Build_Wall(int cnt) {
+    if (cnt == 3) { // 벽 3개를 세우고 BFS
+        vec.assign(arr.begin(), arr.end());
+        answer = max(BFS(), answer);
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (!arr[i][j]) {
+                arr[i][j] = 1;
+                if (cnt < 3) Build_Wall(cnt+1);
+                arr[i][j] = 0; // 사용 후 초기화
             }
         }
     }
 }
 
 int main() {
-    int answer = 0;
-    cin >> n >> m;
+    scanf("%d %d", &n, &m);
 
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) cin >> arr[i][j];
+        for (int j = 0; j < m; j++) {
+            scanf("%d", &arr[i][j]);
+            if (arr[i][j] == 2) virus.push_back(make_pair(i, j)); // 바이러스 위치 정보 저장
+        }
     }
 
-    bool check = true;
-    int num = 0;
-    while(check) {
-        vec.assign(arr.begin(), arr.end());
-        memset(visited, 0, sizeof(visited));
-
-        int cnt = 0;
-        for (int i = (num - 1) / n; i < n; i++) {
-            for (int j = (num + 1) % m; j < m; j++) {
-                if (!vec[i][j]) { vec[i][j] = 1; cnt++; }
-                if (i == n - 1 && j == m - 1) check = false;
-                if (cnt == 3)  break;
-            }
-            if (cnt == 3) { num++; break; }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (!arr[i][j]) {
+                arr[i][j] = 1;
+                Build_Wall(1);
+                arr[i][j] = 0; // 사용 후 초기화
+                }
         }
-        cout << num << endl;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                cout << vec[i][j] << " ";
-            }
-            cout << endl;
-        }
-        cout << endl;
-
-        // if (cnt == 3) {
-        //     for (int i = 0; i < n; i++) {
-        //         for (int j = 0; j < m; j++) {
-        //             if (vec[i][j] == 2 && !visited[i][j]) BFS(make_pair(i, j));
-        //         }
-        //     }
-
-        //     int temp = 0;
-        //     for (int i = 0; i < n; i++) {
-        //         for (int j = 0; j < m; j++) {
-        //             cout << vec[i][j] << " ";
-        //             if (!vec[i][j]) temp++;
-        //         }
-        //         cout << endl;
-        //     }
-        //     cout << endl;
-
-        //     if (temp > answer) answer = temp;
-        // }
     }
-
-    cout << answer << endl;
+    
+    printf("%d\n", answer);
 }
